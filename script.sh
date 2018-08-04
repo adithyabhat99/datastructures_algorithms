a244c149879449da4ce6efcaa6cc2fd7edca4dd3
adithyabhat99 <adithyabhatoct@gmail.com>
2018-08-04 09:41:38 +0530

    Added new shell script

---
+++ script.sh
@@ -0,0 +1,10 @@
+echo 'Shell script is working'
+echo ''
+echo 'your home directory contains these folders and files'
+echo ''
+cd
+ls
+echo ''
+echo 'Your music files are stored in ~/music.txt '
+cd ~/Music
+ls>~/music.txt
