2c02521850310152d2576433cff788f3ce548623
adithyabhat99 <adithyabhatoct@gmail.com>
2018-11-11 19:10:01 +0530

    hi

--- gitpush.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash  
-git add . && \
-git add -u && \
-read -p "Commit description: " desc  
-git commit -m '$desc'  
-curl -u '[adithyabhat99]:[Ma*123654654]' https://api.github.com/user/
-git push origin master
