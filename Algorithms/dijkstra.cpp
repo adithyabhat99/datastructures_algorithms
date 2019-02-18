4f451f15650a26a6e63fca1ffab3bb3fa35b596d
adithyabhat99 <adithyabhatoct@gmail.com>
2019-02-18 14:14:54 +0530

    changes in dij.cpp

--- Algorithms/dijkstra.cpp
+++ Algorithms/dijkstra.cpp
@@ -1,6 +1,19 @@
 /*
 Test case
 9
+999 4 999 999 999 999 999 8 999    
+4 999 8 999 999 999 999 11 999
+999 8 999 7 999 4 999 999 2
+999 999 999 7 0 9 14 999 999
+999 999 999 9 999 10 999 999 999 
+999 999 4 14 10 999 2 999 999 
+999 999 999 999 999 2 999 1 6
+8 11 999 999 999 999 1 999 7
+999 999 2 999 999 999 6 7 999
+
+*/
+/*
+9
 0 4 0 0 0 0 0 8 0    
 4 0 8 0 0 0 0 11 0
 0 8 0 7 0 4 0 0 2
@@ -10,6 +23,7 @@ Test case
 0 0 0 0 0 2 0 1 6
 8 11 0 0 0 0 1 0 7
 0 0 2 0 0 0 6 7 0
+if you takedis[i]=999 initially and all matrix entries are 0 for no direct edges
 */
 
 #include<iostream>
@@ -29,7 +43,7 @@ void dij(int **a,int src,int n){
     int dis[n],i,u,w,count;
     int v[n];
     for(i=0;i<n;i++){
-        dis[i]=999;
+        dis[i]=a[src][i]; //dis[i]=999 if you assume 0 in a[][] as no direct edge
         v[i]=0;
     }
     dis[src]=0;
@@ -37,7 +51,7 @@ void dij(int **a,int src,int n){
         int u=minW(dis,v,n);
         v[u]=1;
         for(w=0;w<n;w++){
-            if(dis[w]>dis[u]+a[u][w] && !v[w] && a[u][w])
+            if(dis[w]>dis[u]+a[u][w] && !v[w])  // &&a[u][w] if you assume 0 in a[][] as no direct edge
             dis[w]=dis[u]+a[u][w];
         }
     }
